fb61b2a933dd6e64b627a73fbef9434b432339e3
sofia <sofi4rodriguez5@gmail.com>
2026-05-26 08:34:51 -0500

    cambios

---
+++ nodejs-tests/Resta-Multiplicacion.js
@@ -0,0 +1,9 @@
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { substract, multiply };
---
+++ nodejs-tests/__Test__/Resta-Multiplicacion.test.js
@@ -0,0 +1,28 @@
+const { substract, multiply } = require('../Resta-Multiplicacion');
+
+describe('mathHelper', () => {
+
+  // describe() agrupa tests relacionados
+  describe('substract()', () => {
+    it('resta dos números positivos correctamente', () => {
+      // Arrange
+      const a = 5, b = 3;
+
+      // Act
+      const result = substract(a, b);
+
+      // Assert
+      expect(result).toBe(2);
+    });
+
+    it('resta números negativos', () => {
+      expect(substract(-1, -4)).toBe(3);
+    });
+  });
+
+  describe('multiply()', () => {
+    it('multiplica dos números positivos correctamente', () => {
+      expect(multiply(2, 3)).toBe(6);
+    });
+  });
+});
